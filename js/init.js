3e4d6ae77d391d572be15994a0690d3e49727293
Serge Ballif <ballifserge@gmail.com>
2016-12-19 17:43:47 -0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -16,7 +16,7 @@
 				global: {
 					range: '*',
 					href: '{{ site.baseurl }}/css/style.css',
-					containers: 2000,//1400
+					containers: 2700,//1400
 					grid: {
 						gutters: {
 							vertical: '4em',
