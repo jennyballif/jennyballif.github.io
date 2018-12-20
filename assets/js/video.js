500799ad0076028ac42a3870881558f4e37a81ad
Serge Ballif <ballifserge@gmail.com>
2018-12-20 09:24:44 -0800

    greetings
    greetings

--- assets/js/video.js
+++ assets/js/video.js
@@ -4,7 +4,13 @@
 
 	for (var i = 0; i < youtube.length; i++) {
 
-		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+		var loc = window.location.toString();
+		var params = loc.split('?')[1];
+		if (youtube[i].dataset.embed) {
+		  params = youtube[i].dataset.embed;
+		}
+
+		var source = "https://img.youtube.com/vi/"+ params +"/sddefault.jpg";
 
 		var image = new Image();
 				image.src = source;
@@ -14,11 +20,17 @@
 
 				youtube[i].addEventListener( "click", function() {
 
+					var loc = window.location.toString();
+					var param = loc.split('?')[1];
+					if (this.dataset.embed) {
+					  param = this.dataset.embed;
+					}
+
 					var iframe = document.createElement( "iframe" );
 
 							iframe.setAttribute( "frameborder", "0" );
 							iframe.setAttribute( "allowfullscreen", "" );
-							iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+							iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ param +"?rel=0&showinfo=0&autoplay=1" );
 
 							this.innerHTML = "";
 							this.appendChild( iframe );
