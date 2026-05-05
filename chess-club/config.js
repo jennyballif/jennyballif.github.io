04720548179a1d69c4878c8e31efc8a8bb0eeca8
Serge Ballif <jenballif@gmail.com>
2026-05-05 15:17:37 -0800

    added a single comma

--- chess-club/config.js
+++ chess-club/config.js
@@ -1,6 +1,6 @@
 // Chess Club configuration
 // Edit socketUrl to point to your deployed backend.
 window.CHESS_CLUB_CONFIG = {
-  socketUrl: 'https://chess-club-backend-0mbw.onrender.com'
+  socketUrl: 'https://chess-club-backend-0mbw.onrender.com',
   teacherPasswordEnabled: true  // set to true if you've set TEACHER_PASSWORD on your Render backend
 };
